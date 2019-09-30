9bbf052b24ef59dc733c07e06bd991eaecc188b7
Ricardo <ricardo.ferreiraribeiro@protonmail.com>
2019-09-30 16:04:40 +0200

    Start implementing the main
    This creates the main program called gossiper. It allow the user to
    create a gossiper with some properties defined in peer/gossiper.go and
    to check those properties with the functions in utils/network.go.

--- main.go
+++ main.go
@@ -1,1 +1,60 @@
-package Peerster
+package main
+
+import (
+	"Peerster/peer"
+	"Peerster/utils"
+	"flag"
+	"fmt"
+	"strings"
+)
+
+var (
+	uiport     string
+	gossipAddr string
+	name       string
+	peersStr   string
+	peers      []string
+	simple     bool
+	gossiper   peer.Gossiper
+)
+
+func init() {
+	flag.StringVar(&uiport, "UIPort", "8080", "port for the UI client (default \"8080\")")
+	flag.StringVar(&gossipAddr, "gossipAddr", "127.0.0.1:5000", "ip:port for the gossiper (default \"127.0.0.1:5000\"")
+	flag.StringVar(&name, "name", "default", "name of the gossiper")
+	flag.StringVar(&peersStr, "peers", "", "comma separated list of peers of the form ip:port")
+	flag.BoolVar(&simple, "simple", false, "run gossiper in simple broadcast mode")
+	flag.Parse()
+
+	if !utils.ValidPort(uiport) {
+		panic("In Gossiper: UIPort is not a valid port")
+	}
+
+	if !utils.ValidPortIP(gossipAddr) {
+		panic("In Gossiper: gossipAddr is not a valid pair ip:port")
+	}
+
+	peers = make([]string, 0)
+	if len(peersStr) != 0 {
+		splitPairs := strings.Split(peersStr, ",")
+
+		for _, pair := range splitPairs {
+			if utils.ValidPortIP(pair) {
+				peers = append(peers, pair)
+			}
+		}
+	}
+
+	gossiper = peer.Gossiper{
+		UIPort:     uiport,
+		GossipAddr: gossipAddr,
+		Name:       name,
+		Peers:      peers,
+		Simple:     simple,
+	}
+
+}
+
+func main() {
+	fmt.Println(&gossiper)
+}
---
+++ peer/gossiper.go
@@ -0,0 +1,39 @@
+package peer
+
+import "fmt"
+
+type Gossiper struct {
+	UIPort     string
+	GossipAddr string
+	Name       string
+	Peers      []string
+	Simple     bool
+}
+
+func (g *Gossiper) String() string {
+	s := fmt.Sprintf("Name: %s\nUIPort: %s\nGossipAddr: %s\n", g.Name, g.UIPort, g.GossipAddr)
+
+	if g.Simple {
+		s += "Simple: yes\n"
+	} else {
+		s += "Simple: no\n"
+	}
+
+	s += "Peers:"
+
+	if len(g.Peers) == 0 {
+		s += " no peer"
+	} else {
+		s += "\n"
+		for i, peer := range g.Peers {
+			if i != len(g.Peers)-1{
+				s += "- " + peer + "\n"
+			}else{
+				s += "- " + peer
+			}
+
+		}
+	}
+
+	return s
+}
